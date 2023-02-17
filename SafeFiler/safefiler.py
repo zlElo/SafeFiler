f240adf4fb2538780b8c663a9ef6eb95ebec5d06
zlElo <108819252+zlElo@users.noreply.github.com>
2023-02-17 10:21:50 +0100

    Add GUI

--- SafeFiler/safefiler.py
+++ SafeFiler/safefiler.py
@@ -1,118 +1,138 @@
 import os
-from time import sleep
-from subprocess import call
+from time import sleep 
+import tkinter as tk
+from tkinter import ttk
+from tkinter import * 
+from tkinter import filedialog
 
-def encrypt(file): # Your file will be encryptet
+
+def key_delete():
+    root = Tk()
+
+    # This is the section of code which creates the main window
+    root.geometry('389x114')
+    root.configure(background='#F0F8FF')
+    root.title('key devalued')
+
+
+    # This is the section of code which creates the a label
+    Label(root, text='The key for your file is devalued.', bg='#F0F8FF', font=('arial', 12, 'normal')).place(x=76, y=27)
+
+
+    # This is the section of code which creates the a label
+    Label(root, text='You can delete the key now!', bg='#F0F8FF', font=('arial', 12, 'normal')).place(x=98, y=63)
+
+
+    root.mainloop()
+
+def encrypt_done():
+    root = Tk()
+
+    # This is the section of code which creates the main window
+    root.configure(background='#F0F8FF')
+    root.title('encrypted')
+
+
+    # This is the section of code which creates the a label
+    Label(root, text='The selected file is encrypted!', bg='#F0F8FF', font=('arial', 12, 'normal')).pack()
+
+
+    # This is the section of code which creates the a label
+    Label(root, text='The key to decrypt the file is there, where the encrypted file is.', bg='#F0F8FF', font=('arial', 12, 'normal')).pack()
+
+
+    root.mainloop()
+
+def encrypt(): # Your file will be encrypted
+    file = filedialog.askopenfilename(title='Select a file')
     have_to_encrypt = open(file, "rb").read()
     size = len(have_to_encrypt)
+
+    progessBar['value'] = 20
+    root.update_idletasks()
+
     key = os.urandom(size)
+
+    progessBar['value'] = 40
+    root.update_idletasks()
+
     with open(f'{file}.key', "wb") as key_out:
         key_out.write(key)
-    encryptet = bytes(a ^ b for (a, b) in zip(have_to_encrypt, key))
-    with open(file, "wb") as encryptet_out:
-        encryptet_out.write(encryptet)
-    print('The selected file is encryptet! The key to decrypt the file is there, where the encryptet file is.')
-    sleep(3)
+    progessBar['value'] = 60
+    root.update_idletasks()
 
-def decrypt(filename, key): # Your file will be decryptet
+    encrypted = bytes(a ^ b for (a, b) in zip(have_to_encrypt, key))
+
+    progessBar['value'] = 80
+    root.update_idletasks()
+
+    with open(file, "wb") as encrypted_out:
+        encrypted_out.write(encrypted)
+
+    progessBar['value'] = 100
+    root.update_idletasks()
+
+    encrypt_done()
+
+
+def decrypt(): # Your file will be decryptet
+    filename = filedialog.askopenfilename(title='Select the encrypted file')
+    key = filedialog.askopenfilename(title='Select the key for the encrypeted file')
     file = open(filename, "rb").read()
+
+    progessBar['value'] = 30
+    root.update_idletasks()
+
     key = open(key, "rb").read()
     decrypted = bytes(a ^ b for (a, b) in zip(file, key))
+
+    progessBar['value'] = 60
+    root.update_idletasks()
+
     with open(filename, "wb") as decrypted_out:
         decrypted_out.write(decrypted)
-    sleep(1)
-    print('')
-    print('The key for your file is devalued, you can delet the key now!')
-    print('')
-    sleep(2)
-    startq = input('Do you want to go to the startpage? (y/n): ')
-    if startq == 'y':
-        freshConsole()
-        call(['python', 'safefiler.py'])
-    if startq == 'n':
-        print('')
-        print('Bye!')
-        sleep(1)
+    
+    progessBar['value'] = 100
+    root.update_idletasks()
 
-# the Def for the other files to clear the console
-def freshConsole():
-    clear = lambda: os.system('cls' if os.name in ('nt', 'dos') else 'clear')
-    clear()
-
-def aboutq():
-    freshConsole()
-    print("""
-
-This Program stays under the MIT Licens!
+    # call the done/key delete window
+    key_delete()
+    
 
 
-Created by zlELo
 
-Developer on GitHub: github.com/zlElo
-Developer Homepage: zlelo.github.io
-
-    """)
-
-    back_quest = input('Do you want to go back to the Startpage? (y/n): ')
-
-    if back_quest == 'y':
-        freshConsole()
-        call(['python', 'safefiler.py'])
-    elif back_quest == 'n':
-        print('Have a nice day, Bye!')
-        sleep(0.5)
-        quit()
-
-# The startpage
-print("""
-                       _____       ____     _______ __         
-                      / ___/____ _/ __/__  / ____(_) /__  _____
-                      \__ \/ __ `/ /_/ _ \/ /_  / / / _ \/ ___/
-                     ___/ / /_/ / __/  __/ __/ / / /  __/ /    
-                    /____/\__,_/_/  \___/_/   /_/_/\___/_/     
-                    by zlElo
-
-Welcome to SafeFiler, your program to encrypt and decrypt important files
-secure and fast!
+# This is a function which increases the progress bar value by the given increment amount
+def makeProgress():
+	progessBar['value']=progessBar['value'] + 1
+	root.update_idletasks()
 
 
-You have this functions:
 
-- encrypt, encrypt files with a very high security (command: encrypt)
-- decrypt, decrypt the encrpyted file with the generated key (command: decrypt)
+root = Tk()
 
-More:
-
-- About (command: about)
-- Close the program (command: bye)
-
-""")
+# This is the section of code which creates the main window
+root.geometry('297x119')
+root.configure(background='#F0F8FF')
+root.title('SafeFiler')
 
 
-# start of name process
-file_size = os.path.getsize('name.txt')
-if file_size == 0:
-    entered_name = input('Hey, you are new here. Pleas enter your Name: ')
-    file1 = open("name.txt", "w") 
-    file1.write(entered_name)
-    file1.close()
-
-f = open('name.txt', 'r')
-contents = f.read()
-name = contents
-# End of name process
+# This is the section of code which creates a button
+Button(root, text='Encrypt', bg='#F0F8FF', font=('arial', 12, 'normal'), command=encrypt).place(x=22, y=26)
 
 
-start = input(f'Hello {name}, what do you want to do?: ')
-if start == 'encrypt':
-    file = input('Pleas Enter the Path to the file: ')
-    encrypt(file)
-if start == 'decrypt':
-    filename = input('Pleas Enter the Path to the file: ')
-    key = input('Pleas Enter the Path of the key for the file: ')
-    decrypt(filename, key)
-if start == 'about':
-    aboutq()
-if start == 'bye':
-    print('Bye!')
-    quit()
+# This is the section of code which creates a button
+Button(root, text='Decrypt', bg='#F0F8FF', font=('arial', 12, 'normal'), command=decrypt).place(x=121, y=26)
+
+
+# This is the section of code which creates a color style to be used with the progress bar
+progessBar_style = ttk.Style()
+progessBar_style.theme_use('clam')
+progessBar_style.configure('progessBar.Horizontal.TProgressbar', foreground='#F0F8FF', background='#F0F8FF')
+
+
+# This is the section of code which creates a progress bar
+progessBar=ttk.Progressbar(root, style='progessBar.Horizontal.TProgressbar', orient='horizontal', length=252, mode='determinate', maximum=100, value=1)
+progessBar.place(x=21, y=82)
+
+
+root.mainloop()
